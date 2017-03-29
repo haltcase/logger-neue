a24cc55e3f384bfe3b6421024d3fbdfe2ea472d0
citycide <thecitycide@gmail.com>
2017-03-29 00:09:59 -0500

    chore(eslint): add lightscript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
+const OFF = 0
+const WARN = 1
+const ERROR = 2
+
 module.exports = {
   root: true,
+  parser: 'lightscript-eslint',
   parserOptions: {
     sourceType: 'module'
   },
   env: {
-    browser: true,
     node: true
   },
   extends: 'standard',
   plugins: [],
   rules: {
-    quotes: ['error', 'single', {
-      avoidEscape: true,
-      allowTemplateLiterals: true
-    }]
+    quotes: [ERROR, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'generator-star-spacing': [ERROR, { before: true, after: true }],
+    'array-bracket-spacing': [ERROR, 'never'],
+    'object-shorthand': [ERROR, 'always'],
+    'arrow-parens': [ERROR, 'as-needed'],
+    'comma-dangle': ERROR,
+    'no-debugger': process.env.NODE_ENV === 'production' ? ERROR : OFF,
+    'object-curly-spacing': [ERROR, 'always'],
   },
-  globals: {
-    define: false
-  }
+  settings: {}
 }
